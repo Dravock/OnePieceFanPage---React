02a6551c1624696880129999f4c05f56115a5152
Deryan Keskin <Deryan@keskin-neubulach.de>
2023-07-20 22:31:52 +0200

    desgined frontend

--- client/src/Pages/Public/AppS.js
+++ client/src/Pages/Public/AppS.js
@@ -1,21 +1,19 @@
 import React, { useState } from 'react'
 import luffy from '../../includes/img/png/characters/luffy/luffy.png'
 import Text_with_IMG from '../../components/Text_with_IMG/Text_with_IMG'
+import Hero from "../../components/Herobanner/Hero.js"
 
 function App() {
   
   return (
-    <div className="App bg-wood p-4">
-        <section>
-          <main>
-            <div className='bg-paper-wood w-1/3  border-red-700 border-4 p-4 '>
-              <img src={luffy} className='aspect-[4/8]' title='' alt='' width='100%' />
-            </div>
-          </main>
+    <div className="App bg-wood">
+        <section className='p-0'>
+            <Hero height={"300px"} imgSRC={"https://wallpapercrafter.com/th800/99564-One-Piece-Monkey-D.-Luffy-anime-anime-boys.jpg"} space={"mb-4"} text={"Monkey D. Luffy / Gear 4 - Snakeman"}/>
         </section>
 
         <section>
-          <Text_with_IMG headline={"Test Headlin"} text={"Lorem Ipsum"} pos={""} space={"mb-5"} imgSRC={""} altDesc={"Nami IMG"} />
+          <Text_with_IMG headline={"Test Headlin"} text={"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."} pos={""} space={"mb-5"} imgSRC={""} altDesc={"Nami IMG"} />
+          <Text_with_IMG headline={"Test Headlin"} text={"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."} pos={"text-left"} space={"mb-5"} imgSRC={""} altDesc={"Nami IMG"} />
         </section>
         
         <section>
--- client/src/components/Herobanner/Hero.js
+++ client/src/components/Herobanner/Hero.js
@@ -2,16 +2,20 @@ import React from 'react'
 
 
 function Hero(props) {
-    const { imgSRC,text,space } = props
+    const { imgSRC,text,space,height } = props
+
+    const imgSRC2 = "https://www.tributemedia.com/hubfs/Images/Blog%20Images/shutterstock_252081805.jpg"
 
 return (
-    <section id="HeroBanner" className={space}>
-        <div style={{backgroundImage:`url(${imgSRC})`}} className="relative hero-banner">
-            <div className='absolute bottom-5 sm:bottom-10 bg-black/80 w-fit  ml-4 px-4'>
-                <h2 className='text-xs sm:text-xl font-bold text-white'>{text}</h2>
+    <main>
+        <div id="HeroBanner" className={space}>
+            <div style={{backgroundImage:`url(${imgSRC})`,height:`${height}`}} className="relative hero-banner h-[450px] bg-cover bg-no-repeat bg-center">
+                <div className='absolute bottom-5 sm:bottom-10 bg-black/80 w-fit  ml-4 px-4'>
+                    <h2 className='text-xs sm:text-xl font-bold text-white'>{text}</h2>
+                </div>
             </div>
         </div>
-    </section>
+    </main>
 )}
 
 export default Hero
