fa71dd7647a4927b6388cc05fbcade05a8166b23
Deryan Keskin <Deryan@keskin-neubulach.de>
2023-07-20 21:19:55 +0200

    changed type of tag to file

--- client/src/backend/components/rp_media/MediaUpload/ImageUploader.js
+++ client/src/backend/components/rp_media/MediaUpload/ImageUploader.js
@@ -9,7 +9,7 @@ function ImageUploader() {
                 <div className=' border-2 border-black border-dotted px-16 py-4 my-4 '>
                     <h2 className='mt-4 mb-2  text-2xl font-bold'>Bild hochladen</h2>
                     <h3 className='mb-4 text-xl'>Klicken Sie auf den Button <b>"+"</b> um Ihre Bilder vom Gerät hochzuladen. </h3>
-                    <button className='bg-green-600/80 hover:bg-green-700/80 rounded-xl p-2 text-white w-16 mb-4'><FontAwesomeIcon icon={faPlus} className='font-bold'/></button>
+                    <input type="file" className='bg-green-600/80 hover:bg-green-700/80 rounded-xl p-2 text-white w-16 mb-4' /><FontAwesomeIcon icon={faPlus} className='font-bold'/>
                 </div>
             </div>
         </section>
